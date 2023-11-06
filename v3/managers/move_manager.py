ef1a3d165bd42182c66dc7031d29e39ad21fe666
pnikitits <112090831+pnikitits@users.noreply.github.com>
2023-11-06 11:19:55 +0100

    Update move_manager with new strategy

--- v3/managers/move_manager.py
+++ v3/managers/move_manager.py
@@ -1,4 +1,5 @@
 from move_algorithms.move_template import compute_move_random
+from move_algorithms.move_file_1 import *
 
 
 class MoveManager:
@@ -9,5 +10,6 @@ class MoveManager:
     def compute_move(self , strategy , map_lists , grid):
         if strategy == "random":
             return compute_move_random(map_lists , grid)
+        elif strategy == "seek":
+            return compute_move_seek(map_lists , grid)
         # List the strategies here
-
