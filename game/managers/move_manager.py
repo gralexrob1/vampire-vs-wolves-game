48e71019e6ad57d8f7ff7fc7b596827d839e1e5c
Alexandre Robin <alexandre.robin81@gmail.com>
2023-11-14 16:22:54 +0100

    Alpha Beta first intuition

--- game/managers/move_manager.py
+++ game/managers/move_manager.py
@@ -1,15 +1,23 @@
 from move_algorithms.move_template import compute_move_random
 from move_algorithms.move_file_1 import *
+from move_algorithms.alpha_beta import *
 
 
 class MoveManager:
+
     def __init__(self):
         pass
 
 
     def compute_move(self , strategy , map_lists , grid):
+
         if strategy == "random":
             return compute_move_random(map_lists , grid)
+        
         elif strategy == "seek":
             return compute_move_seek(map_lists , grid)
+        
+        elif strategy == "humanless":
+            return compute_move_alpha_beta(map_lists, grid, "humanless")
+        
         # List the strategies here
