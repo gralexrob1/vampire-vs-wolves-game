eef361f14ca78c501b27ff830277aaef4754e728
pnikitits <112090831+pnikitits@users.noreply.github.com>
2023-12-19 09:26:37 +0100

    Update move_manager.py

--- game/managers/move_manager.py
+++ game/managers/move_manager.py
@@ -13,19 +13,6 @@ class MoveManager:
 
 
     def compute_move(self , strategy , map_lists , grid):
-
-        if strategy == "random":
-            return compute_move_random(map_lists , grid)
+        return compute_move_expect(map_lists , grid)
         
-        elif strategy == "seek":
-            return compute_move_seek(map_lists , grid)
-          
-        elif strategy == "expect":
-            return compute_move_expect(map_lists , grid)
-          
-        elif strategy == "ab_pierre":
-            return compute_move_ab(map_lists , grid)
-        
-        elif strategy == "humanless":
-            return compute_move_alpha_beta(map_lists, grid, "humanless")
         
